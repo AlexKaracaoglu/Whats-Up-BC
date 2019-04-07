7918b931973e37127c43cd69a4f8113f613e395f
Alex Karacaoglu <alexkaracaoglu@alexs-mbp.home>
2019-04-06 21:55:47 -0400

    Initial Commit

---
+++ WhatsUpBC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsUpBC
+//
+//  Created by Alex Karacaoglu on 4/6/19.
+//  Copyright © 2019 Alex Karacaoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
